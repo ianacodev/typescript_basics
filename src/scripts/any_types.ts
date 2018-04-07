fc84fba07450302ccd41b5cbdebfdc1d870a42c7
ianacodev <ianaco.dev@gmail.com>
2018-04-07 02:01:30 -0400

    additional scripts

---
+++ src/scripts/any_types.ts
@@ -0,0 +1,10 @@
+/*****************************
+ * Any Types
+ * Dont know what argument type is.
+ *****************************/
+let age: any;
+age = 25;
+age = '25';
+age = true;
+
+// migrating js project to ts, may use any.
